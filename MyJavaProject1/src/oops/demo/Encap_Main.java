59a01e49caecd6d1b95a8f7631cd73f363da40c7
Qamreen <117499080+Qamreen22@users.noreply.github.com>
2025-03-13 19:50:14 +0530

    day2 commit

---
+++ MyJavaProject1/src/oops/demo/Encap_Main.java
@@ -0,0 +1,15 @@
+package oops.demo;
+
+public class Encap_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EncapDemo obj = new EncapDemo();
+		obj.setSerialNum(101);
+		obj.setName("Bunny");
+		obj.setAge(15);
+		System.out.println(obj);
+
+	}
+
+}
