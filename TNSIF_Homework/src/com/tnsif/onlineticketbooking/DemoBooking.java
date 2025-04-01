6615c9a2c9a2c2eba5958c8612d108122df146a1
Qamreen <117499080+Qamreen22@users.noreply.github.com>
2025-04-01 21:17:33 +0530

    update

--- TNSIF_Homework/src/com/tnsif/onlineticketbooking/DemoBooking.java
+++ TNSIF_Homework/src/com/tnsif/onlineticketbooking/DemoBooking.java
@@ -3,11 +3,21 @@ package com.tnsif.onlineticketbooking;
 public class DemoBooking {
 
 	public static void main(String[] args) {
-		TicketBooking book = new TicketBooking();
+		TicketBooking book[] = new TicketBooking[11];
 		for(int i = 0; i<=10; i++) {
-			book.booking();
+			book[i] = new TicketBooking();
+			book[i].booking();
 			TicketBooking.display();
 		}
 	}
 
 }
+
+/*
+for(int i = 0; i<=10; i++) {
+		TicketBooking book = new TicketBooking();
+		book.booking();
+		TicketBooking.display();
+}
+This creates 10 obj but with same name so we can use array instead to be able to refer to the all the objects later on
+*/
