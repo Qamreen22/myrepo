f5a0a639703cfc660979cf9084830cbddad41dc6
Qamreen <117499080+Qamreen22@users.noreply.github.com>
2025-04-15 19:05:54 +0530

    new

--- B09/src/com/tnsif/exceptionHandling/CheckedExample.java
+++ B09/src/com/tnsif/exceptionHandling/CheckedExample.java
@@ -5,10 +5,10 @@ import java.io.*;
 public class CheckedExample {
 
 	public static void main(String[] args) {
-			FileReader file = new FileReader("data.txt");
-			BufferedReader fileInput = new BufferedReader(file);
-			System.out.println(fileInput.readLine());
-			fileInput.close();
+			//FileReader file = new FileReader("data.txt");
+			//BufferedReader fileInput = new BufferedReader(file);
+			//System.out.println(fileInput.readLine());
+			//fileInput.close();
 	}
 
 }
