057d2c696411c691377dfe03fce1c0d40341407e
Qamreen <117499080+Qamreen22@users.noreply.github.com>
2025-04-15 11:42:14 +0530

    errors and exceptions

---
+++ B09/src/com/tnsif/exceptionHandling/CheckedExample.java
@@ -0,0 +1,14 @@
+package com.tnsif.exceptionHandling;
+
+import java.io.*;
+
+public class CheckedExample {
+
+	public static void main(String[] args) {
+			FileReader file = new FileReader("data.txt");
+			BufferedReader fileInput = new BufferedReader(file);
+			System.out.println(fileInput.readLine());
+			fileInput.close();
+	}
+
+}
